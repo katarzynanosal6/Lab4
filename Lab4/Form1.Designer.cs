1c1d9af87e5066862f180664f67554b6e650dfe2
katarzynanosal6 <katarzynan101@wp.pl>
2026-04-08 20:39:29 +0200

    rotate commit

--- Lab4/Form1.Designer.cs
+++ Lab4/Form1.Designer.cs
@@ -28,12 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            pictureBox1 = new PictureBox();
+            btnLoad = new Button();
+            rb90 = new RadioButton();
+            rb180 = new RadioButton();
+            rb270 = new RadioButton();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(398, 65);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(314, 348);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // btnLoad
+            // 
+            btnLoad.Location = new Point(75, 373);
+            btnLoad.Name = "btnLoad";
+            btnLoad.Size = new Size(131, 40);
+            btnLoad.TabIndex = 1;
+            btnLoad.Text = "Load";
+            btnLoad.UseVisualStyleBackColor = true;
+            btnLoad.Click += btnLoad_Click;
+            // 
+            // rb90
+            // 
+            rb90.AutoSize = true;
+            rb90.Location = new Point(65, 45);
+            rb90.Name = "rb90";
+            rb90.Size = new Size(60, 34);
+            rb90.TabIndex = 2;
+            rb90.TabStop = true;
+            rb90.Text = "90";
+            rb90.UseVisualStyleBackColor = true;
+            // 
+            // rb180
+            // 
+            rb180.AutoSize = true;
+            rb180.Location = new Point(65, 85);
+            rb180.Name = "rb180";
+            rb180.Size = new Size(71, 34);
+            rb180.TabIndex = 3;
+            rb180.TabStop = true;
+            rb180.Text = "180";
+            rb180.UseVisualStyleBackColor = true;
+            // 
+            // rb270
+            // 
+            rb270.AutoSize = true;
+            rb270.Location = new Point(65, 125);
+            rb270.Name = "rb270";
+            rb270.Size = new Size(71, 34);
+            rb270.TabIndex = 4;
+            rb270.TabStop = true;
+            rb270.Text = "270";
+            rb270.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(54, 165);
+            button1.Name = "button1";
+            button1.Size = new Size(131, 40);
+            button1.TabIndex = 5;
+            button1.Text = "Rotate";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(rb270);
+            Controls.Add(rb180);
+            Controls.Add(rb90);
+            Controls.Add(btnLoad);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Button btnLoad;
+        private RadioButton rb90;
+        private RadioButton rb180;
+        private RadioButton rb270;
+        private Button button1;
     }
 }
--- Lab4/Form1.cs
+++ Lab4/Form1.cs
@@ -6,5 +6,23 @@ namespace Lab4
         {
             InitializeComponent();
         }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog { Filter = "Bitmap Files|*.bmp" };
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = new Bitmap(ofd.FileName);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null) return;
+            if (rb90.Checked) pictureBox1.Image.RotateFlip(RotateFlipType.Rotate90FlipNone); 
+            else if (rb180.Checked) pictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipNone);
+            else if (rb270.Checked) pictureBox1.Image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+            pictureBox1.Refresh();
+        }
     }
 }
